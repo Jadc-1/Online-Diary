2943dde7e503aaf231d03a7a93fb14a70b467691
João Alexandre <joaoale.25@gmail.com>
2025-01-10 00:04:17 -0300

    Started database config

--- diario/models.py
+++ diario/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Pessoas(models.Model):
+    nome = models.CharField(max_length=100)
+    foto = models.ImageField(upload_to='foto')
+
+    def __str__(self):
+        return self.nome
